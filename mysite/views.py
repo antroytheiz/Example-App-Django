cebf0685a6248cc6e05f59b73a279821bfeeb0f6
antroytheiz <salupacode@gmail.com.(none)>
2020-07-09 20:18:40 +0700

    edit navbar,  add admin navs

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def index(request):
     context = {
         'judul':'Home',
-        'page':'Salupa',
+        'page':'Django',
         'banner':'img/banner_home.png',
         'nav':[
             ['/','Home'],
